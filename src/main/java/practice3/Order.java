9d4901ae6b6ea11d147049bdbb30a76ce1ac8463
Andrea <Andrea2000728>
2023-02-28 23:04:51 +0800

    fix: extract method to a separate object

--- src/main/java/practice3/Order.java
+++ src/main/java/practice3/Order.java
@@ -4,42 +4,28 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class Order {
-
     private List<OrderLineItem> orderLineItemList;
     private List<BigDecimal> discounts;
-    private BigDecimal tax;
+    private BigDecimal tax = BigDecimal.valueOf(0.1);
+
+    public List<OrderLineItem> getOrderLineItemList() {
+        return orderLineItemList;
+    }
+
+    public List<BigDecimal> getDiscounts() {
+        return discounts;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
 
     public Order(List<OrderLineItem> orderLineItemList, List<BigDecimal> discounts) {
         this.orderLineItemList = orderLineItemList;
         this.discounts = discounts;
-        this.tax = new BigDecimal("0.1");
     }
 
-    public BigDecimal calculate() {
-        BigDecimal subTotal = new BigDecimal(0);
-
-        subTotal = getSubTotal(subTotal);
-        subTotal = getDiscounts(subTotal);
-
-        tax = getTax(subTotal);
-        return subTotal.add(tax);
-    }
-
-    private BigDecimal getDiscounts(BigDecimal subTotal) {
-        for (BigDecimal discount : discounts) {
-            subTotal = subTotal.subtract(discount);
-        }
-        return subTotal;
-    }
-
-    private BigDecimal getSubTotal(BigDecimal subTotal) {
-        for (OrderLineItem lineItem : orderLineItemList) {
-            subTotal = subTotal.add(lineItem.getPrice());
-        }
-        return subTotal;
-    }
-
-    private BigDecimal getTax(BigDecimal subTotal) {
-        return subTotal.multiply(this.tax);
+    public BigDecimal calculate(){
+        return new PriceCaculator(this).calculate();
     }
 }
--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,40 @@
 package practice3;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PriceCaculator {
+    private List<OrderLineItem> orderLineItemList;
+    private List<BigDecimal> discounts;
+    private BigDecimal tax;
+
+    public PriceCaculator(Order order) {
+        this.orderLineItemList = order.getOrderLineItemList();
+        this.discounts = order.getDiscounts();
+        this.tax = order.getTax();
+    }
+
+    public BigDecimal calculate() {
+        BigDecimal subTotal = new BigDecimal(0);
+
+        subTotal = getSubTotal(subTotal);
+        subTotal = getDiscounts(subTotal);
+
+        tax = subTotal.multiply(this.tax);
+        return subTotal.add(tax);
+    }
+
+    private BigDecimal getDiscounts(BigDecimal subTotal) {
+        for (BigDecimal discount : discounts) {
+            subTotal = subTotal.subtract(discount);
+        }
+        return subTotal;
+    }
+
+    private BigDecimal getSubTotal(BigDecimal subTotal) {
+        for (OrderLineItem lineItem : orderLineItemList) {
+            subTotal = subTotal.add(lineItem.getPrice());
+        }
+        return subTotal;
+    }
 }
